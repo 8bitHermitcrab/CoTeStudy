8d049c46efea1738e27cf22387371ccf4050b7e6
8bithermitcrab <8bithermitcrab@gmail.com>
2022-05-12 08:53:59 +0900

    220512

--- 고인정/Lv1_harshad_num.py
+++ 고인정/Lv1_harshad_num.py
@@ -1,22 +1,20 @@
 # 하샤드 수
 
-x = 13
-# x_10 = x // x
+x = 234
 
-str_x = str(13)
-x_list = []
+answer = ''
 
-for i in range(len(x)):
-    x_list.append(i)
+list_x = list(str(x))
+sum_num = 0
+print(list_x)
 
-print(x_list)
+for i in range(len(list_x)):
+    sum_num += int(list_x[i])
+    print(sum_num)
+    if x % sum_num == 0:
+        answer = True
+    else:
+        answer = False
 
-# for i in range(x_list[0], len(x_list)+1):
-#     x_list[0] // x_list[i]
 
-if sum(x_list) == x :
-    answer = True
-else :
-    answer = False
-
-print(answer)
+# https://wooaoe.tistory.com/73
