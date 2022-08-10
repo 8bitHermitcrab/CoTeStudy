c051705e4ae5230ef7d4a7d07d887cb77a618ab1
8bithermitcrab <8bithermitcrab@gmail.com>
2022-08-10 14:40:43 +0900

    220810

---
+++ 고인정/프로그래머스/Lv2/Lv2_max_min_num.py
@@ -0,0 +1,16 @@
+# 최댓값과 최솟값
+
+# s = "1 2 3 4"
+s = "-1 -2 -3 -4"
+# s = "-1 -1"
+
+def solution(s):
+    # answer = ''
+    s_list = s.split()
+    s_list_int = list(map(int, s_list))
+    s_list.sort()
+    # print(s_list_int)
+    answer = str(min(s_list_int)) + ' ' + str(max(s_list_int))
+    return answer
+
+print(solution(s))
