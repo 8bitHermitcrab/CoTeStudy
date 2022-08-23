aa69ccfc0a88c408c92e72a1409ed543e2d312f7
8bithermitcrab <8bithermitcrab@gmail.com>
2022-08-23 11:13:51 +0900

    220823

---
+++ 고인정/백준/class3/BOJ_1676.py
@@ -0,0 +1,24 @@
+# 팩토리얼 0의 개수
+
+import math
+
+n = int(input())
+num = math.factorial(n)
+
+cnt = 0
+
+str_num = str(num)
+# print(str_num)
+len_num = len(str_num)
+# print(f'len_num = {len_num}')
+
+for i in range(len_num-1, 0, -1):
+    print(f'i = {i}')
+    print(f'str_num[{i}] = {str_num[i]}')
+    if str_num[i] == '0':
+        cnt += 1
+        print(f'cnt = {cnt}')
+    else:
+        continue
+
+print(cnt)
