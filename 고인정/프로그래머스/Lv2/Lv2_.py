597f9ecccf34bbf5df88a66e0496c8497f37f05e
8bithermitcrab <8bithermitcrab@gmail.com>
2023-01-05 21:35:33 +0900

    220105

---
+++ 고인정/프로그래머스/Lv2/Lv2_.py
@@ -0,0 +1,26 @@
+# 프로그래머스 압축
+
+msg = 'TOBEORNOTTOBEORTOBEORNOT'
+
+def solution(msg):
+    answer = []
+    msg_dict = {}
+
+    for i in range(26):
+        msg_dict[chr(65 + i)] = i + 1
+
+    w, c = 0, 0
+    while True:
+        c += 1
+        if c == len(msg):
+            answer.append(msg_dict[msg[w:c]])
+            break
+        if msg[w:c+1] not in msg_dict:
+            msg_dict[msg[w:c+1]] = len(msg_dict) + 1
+            answer.append(msg_dict[msg[w:c]])
+            w = c
+
+    return answer
+
+
+print(solution(msg))
